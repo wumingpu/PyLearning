ea4f739b9c1a692f70ba91db65d39eedc59401cd
mingpu wu corp <wuming3019@126.com>
2016-05-25 20:18:42 +0800

    py learn
    Change-Id: Ib3ac686ff341bde00225b9e6ce40403af11e0ca9

---
+++ src/Test1/__init__.py
@@ -0,0 +1,8 @@
+from pip._vendor.distlib.compat import raw_input
+name = raw_input('What is your name?')
+if name.endswith('tank'):
+    print ('Hello tank')
+elif name.endswith('xiao'):
+    print ('Hello xiao')
+else:
+    print ('Hello Strange')
